5dff00c032322a276adce0e3cdb4644f947b2497
juanrvvigo2 <juanrvvigo2@gmail.com>
2017-11-06 19:34:08 +0100

    holas

---
+++ programa.js
@@ -0,0 +1,18 @@
+"Use strict"
+/**
+ * @fileOverview este archivo contiene ejemplos de funciones con for
+ * @author JuanRodriguez
+ * @version 1.0
+ */
+/**
+ * @description SumaArray suma los valores de un array
+ * @param {Array} numeros array de numeros
+ * @return number
+ */
+function SumaArray(numeros){
+    let suma=0
+    for (let a of numero){
+        suma=suma+a;
+    }
+    return suma
+}
